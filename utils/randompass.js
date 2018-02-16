04b281a261e329710d5dd0d20c66946c7067868c
Arien Kock <arien@positor.nl>
2018-02-16 18:16:04 +0100

    Lazy load password list

--- utils/randompass.js
+++ utils/randompass.js
@@ -3,9 +3,8 @@ const request = require('request-promise-native')
 let res
 
 module.exports = async () => {
-  if (!res) {
-    res = await request.get('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/10_million_password_list_top_100.txt')
-  }
+  res = res || await request.get('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/10_million_password_list_top_100.txt')
+  
   const passwords = res.split('\n')
 
   return passwords[Math.round(Math.random() * passwords.length)]
