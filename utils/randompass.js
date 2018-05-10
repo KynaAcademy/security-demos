2e16595d8c46efcc054e356efe9965f1b40f1606
Wouter de Vos <wouter@codaisseur.com>
2018-05-10 11:14:09 +0200

    Update password list

--- utils/randompass.js
+++ utils/randompass.js
@@ -3,8 +3,8 @@ const request = require('request-promise-native')
 let res
 
 module.exports = async () => {
-  res = res || await request.get('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/10_million_password_list_top_100.txt')
-  
+  res = res || await request.get('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-100.txt')
+
   const passwords = res.split('\n')
 
   return passwords[Math.round(Math.random() * passwords.length)]
