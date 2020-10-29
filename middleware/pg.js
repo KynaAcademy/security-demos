7d21c5b57c3d394d5c7e84450608a1a0c930c2e0
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2020-10-29 21:03:05 +0100

    Support cloudsql

--- middleware/pg.js
+++ middleware/pg.js
@@ -3,7 +3,17 @@ const { Pool } = require('pg')
 const DATABASE_URL = process.env.DATABASE_URL ||
   'postgresql://localhost:5432/security-demos'
 
-const pg = new Pool({ connectionString: DATABASE_URL, debug: true })
+const cloudsql_pool = () => {
+  const dbSocketPath = process.env.DB_SOCKET_PATH || "/cloudsql";
+  return new Pool({
+    user: process.env.DB_USER, // e.g. 'my-user'
+    password: process.env.DB_PASS, // e.g. 'my-user-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
+    host: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+  });
+}
+
+const pg = process.env.CLOUD_SQL_CONNECTION_NAME ? cloudsql_pool() : new Pool({ connectionString: DATABASE_URL, debug: true });
 
 module.exports.pg = pg
 
